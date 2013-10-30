15fdd7b81e23e1b64fc101225bb62db63fb032f5
irenekhan7 <khani3@rpi.edu>
2013-10-30 19:54:33 -0400

    back to old color algorithm

--- Hex/src/com/teamhex/cooler/ColorPaletteGenerator.java
+++ Hex/src/com/teamhex/cooler/ColorPaletteGenerator.java
@@ -1,183 +1,20 @@
 package com.teamhex.cooler;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.Comparator;
-//import java.util.List;
-//import java.util.Map;
-import java.util.HashMap;
-import java.util.Random;
+import java.util.Map;
+import java.util.TreeMap;
 
-//import android.graphics.Bitmap;
-//import android.graphics.Color;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 
-public class ColorPaletteGenerator 
-{
+public class ColorPaletteGenerator {
 
+	
 	/********************************************************************************************
 	 * Begin Color Algorithm Code
 	 *******************************************************************************************/
 	
-	/*private static int dist(int v1, int v2)
-	 *		Helper function that returns the squared Euclidean distance between the AR(Alpha-Red)
-	 *		and GB(Green-Blue) values of the two selected colors. 
-	 *
-	 * Parameters:
-	 * 		int v1 - the first color to be compared
-	 * 		int v2 - the second color to be compared
-	 * 
-	 * Returns:
-	 * 		The squared Euclidean distance between the two colors, using the AR(Alpha-Red) and
-	 * 		GB(Green-Blue) values as input
-	 */
-	private static int dist(int v1, int v2)
-	{
-		return ((v1 >> 16) - (v2 >> 16)) * ((v1 >> 16) - (v2 >> 16)) + 
-			   ((v1 & 0xFFFF) - (v2 & 0xFFFF)) * ((v1 & 0xFFFF) - (v2 & 0xFFFF));
-	}
-	
-	
-	//Helper comparator class that compares two integers based on their hue values.
-	private static Comparator<Integer> HueComparator = new Comparator<Integer>()
-	{
-		public double hue(Integer i)
-		{
-			int r = (i >> 16) & 0xFF;
-			int g = (i >> 8) & 0xFF;
-			int b = i & 0xFF;
-			
-			return Math.atan2(1.73205080757 * (g - b), 2 * r - g - b);
-		}
-		
-	    @Override
-	    public int compare(Integer i1, Integer i2) 
-	    {
-	        return (int)(hue(i1) - hue(i2));
-	    }
-	};
-	
-	
-	/*public static int[] colorAlgorithm(int[] pixels, int k)
-	 *		Computes a color swatch of size <n> based on the input <bitmap> using k-means 
-	 *      clustering.
-	 * 
-	 * Parameters:
-	 * 		int[] pixels - the input used to generate the color swatch
-	 * 		int k		 - the number of colors to be included in the color swatch
-	 * 
-	 * Returns:
-	 * 		An int array of size <k> that contains the colors in the swatch. Because k-means
-	 * 		clustering returns a local (and not a global) minimum, there is no guarantee that
-	 * 		the same color swatch will be returned for any two identical inputs.
-	 * 
-	 * 		If the size of <pixels> is less than <k>, then it is impossible to generate <k>
-	 * 		color schemes from the input and null will be returned.
-	 */
-	public static int[] colorAlgorithm(int[] pixels, int k)
-	{
-		int n = pixels.length;
-		
-		if (k < n)
-		{
-			Random rand = new Random();
-			
-			int[] means = new int[k];
-			
-			//Randomly and independently select k unique indices whose points will be the initial
-			//values for each of the k means.
-			ArrayList<Integer> index = new ArrayList<Integer>();
-			index.ensureCapacity(n);
-			for (int i = 0; i < n; i++)
-			{
-				index.add(i);
-			}
-			Collections.shuffle(index, rand);
-			for (int i = 0; i < k; i++)
-			{
-				means[i] = pixels[index.get(i)];
-			}
-			index.clear();
-
-			//Iterate through the data set. At each iteration, we pair each pixel with the
-			//closest mean based on squared Euclidean distance. The set of these pixels forms a
-			//Voroni diagram in two dimensions. After we have paired each point with a mean, we
-			//then calculate the centroid of each set, which becomes the new mean. We then repeat
-			//this process until none of the pixels change their nearest mean between iterations.
-			ArrayList<Integer>[] sets = (ArrayList<Integer>[]) new ArrayList[k];
-			HashMap<Integer, Integer> pairings = new HashMap<Integer, Integer>();
-			while (true)
-			{
-				int updates = 0;
-				for (int i = 0; i < n; i++)
-				{
-					int min = 0;
-					for (int j = 1; j < k; j++)
-					{
-						if (dist(pixels[i], means[j]) < dist(pixels[i], means[min]))
-						{
-							min = j;
-						}
-					}
-					sets[min].add(pixels[i]);
-					if (pairings.get(i) != min)
-					{
-						updates++;
-					}
-				}
-				
-				if (updates == 0)
-				{
-					break;
-				}
-				
-				for (int i = 0; i < k; i++)
-				{
-					int x = 0; int y = 0;
-					for (Integer j : sets[i])
-					{
-						x += j >> 16;
-						y += j & 0xFFFF;
-						pairings.put(j, i);
-					}
-					x /= sets[i].size();
-					y /= sets[i].size();
-					
-					means[i] = (x << 16) | y;
-					sets[i].clear();
-				}
-			}
-			
-			//Because the clusters are randomly generated, the colors may appear in any order.
-			//We first sort the colors by hue to make the color scheme appear more presentable
-			//to the user.
-			Integer[] colors = new Integer[k];
-			for (int i = 0; i < k; i++)
-			{
-				colors[i] = means[i];
-			}
-			Arrays.sort(colors, HueComparator);
-			
-			for (int i = 0; i < k; i++)
-			{
-				means[i] = colors[i];
-			}
-			return means;
-		}
-		
-		return null;
-	}
-	
-	/********************************************************************************************
-	 * End Color Algorithm Code
-	 *******************************************************************************************/
-
-	
-	/********************************************************************************************
-	 * Begin OLD Color Algorithm Code
-	 *******************************************************************************************/
-	
-	//private static int threshold = 5000;	//Similarity threshold. Colors whose squared difference
+	private static int threshold = 5000;	//Similarity threshold. Colors whose squared difference
 											//with another color in the swatch is less than or equal
 											//to this value are not included in the swatch. Currently
 											//a fixed value, but should ideally be based on the mean
@@ -195,7 +32,7 @@ public class ColorPaletteGenerator
 	 * 		An int array of size <n> that contains the colors in the swatch. The number of colors
 	 *      in the swatch is not necessarily <n>, however (see below).
 	 */
-	/*public static int[] colorAlgorithm(Bitmap bitmap, int n)
+	public static int[] colorAlgorithm(Bitmap bitmap, int n)
 	{
 		int width = bitmap.getWidth();
 		int height = bitmap.getHeight();
@@ -260,10 +97,10 @@ public class ColorPaletteGenerator
 		}
 		
 		return colorScheme;
-	}*/
+	}
 	
 	/********************************************************************************************
-	 * End OLD Color Algorithm Code
+	 * End Color Algorithm Code
 	 *******************************************************************************************/
 	
 	
