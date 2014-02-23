61ea25653a5165ebd2e7cf26618dd08f50f30e85
Ian Kettlewell <ian.kettlewell@gmail.com>
2014-02-23 18:50:41 -0500

    Maybe fixed rotation?

--- Hex/src/com/teamhex/cooler/MainActivity.java
+++ Hex/src/com/teamhex/cooler/MainActivity.java
@@ -28,6 +28,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -110,6 +111,7 @@ public class MainActivity extends Activity implements PreviewCallback{
                 }
             }
         );
+
     }
     
     
@@ -148,6 +150,7 @@ public class MainActivity extends Activity implements PreviewCallback{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
     }
     
     
@@ -182,6 +185,7 @@ public class MainActivity extends Activity implements PreviewCallback{
         	preview.addView(mPreview); // Add the preview back into the view
         }
         mParameters = mCamera.getParameters();
+        setCameraDisplayOrientation(this,Camera.CameraInfo.CAMERA_FACING_BACK, mCamera);
     }
 
     // Release the camera for other applications
@@ -387,6 +391,33 @@ public class MainActivity extends Activity implements PreviewCallback{
         input.close();
         
         return bitmap;
+    }
+    
+    //SOURCE: http://developer.android.com/reference/android/hardware/Camera.html#setDisplayOrientation%28int%29
+    
+    public static void setCameraDisplayOrientation(Activity activity,
+            int cameraId, android.hardware.Camera camera) {
+        android.hardware.Camera.CameraInfo info =
+                new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay()
+                .getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0: degrees = 0; break;
+            case Surface.ROTATION_90: degrees = 90; break;
+            case Surface.ROTATION_180: degrees = 180; break;
+            case Surface.ROTATION_270: degrees = 270; break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+        camera.setDisplayOrientation(result);
     }
     
     // Returns the sample size given a BitmapFactory options, width, and height
